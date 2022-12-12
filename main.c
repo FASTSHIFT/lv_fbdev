d74bc77f99a8cc5e95ff4c1846c6e1ac08d8e211
FASTSHIFT <vifextech@foxmail.com>
2022-12-12 23:53:45 +0800

    refact(lv_fbdev): remove lvgl & lv_drivers

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 /**
  * @file main
  *
@@ -8,15 +7,15 @@
  *      INCLUDES
  *********************/
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
-#include "lvgl/lvgl.h"
-#include "lvgl/demos/lv_demos.h"
 #include "lv_drivers/display/fbdev.h"
 #include "lv_drivers/indev/evdev.h"
+#include "lvgl/demos/lv_demos.h"
+#include "lvgl/lvgl.h"
 
 /*********************
  *      DEFINES
@@ -30,7 +29,7 @@
  *  STATIC PROTOTYPES
  **********************/
 static void lv_port_disp_init(void);
-static void lv_port_indev_init(const char * path, bool show_cursor);
+static void lv_port_indev_init(const char* path, bool show_cursor);
 static void cursor_set_hidden(bool en);
 
 /**********************
@@ -65,32 +64,32 @@ static void cursor_set_hidden(bool en);
  *   GLOBAL FUNCTIONS
  **********************/
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  if(argc != 2) {
-    printf("input evdev device path\n");
+    if (argc < 2) {
+        printf("input evdev path is empty\n");
+    }
+
+    /*Initialize LVGL*/
+    lv_init();
+
+    /*Initialize the HAL (display, input devices, tick) for LVGL*/
+    lv_port_disp_init();
+    cursor_set_hidden(false);
+
+    const char* devpath = argv[1];
+    if (devpath) {
+        lv_port_indev_init(devpath, true);
+    }
+
+    while (1) {
+        /* Periodically call the lv_task handler.
+         * It could be done in a timer interrupt or an OS task too.*/
+        uint32_t sleep_ms = lv_timer_handler();
+        usleep(sleep_ms * 1000);
+    }
+
     return 0;
-  }
-
-  /*Initialize LVGL*/
-  lv_init();
-
-  /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  lv_port_disp_init();
-  lv_port_indev_init(argv[1], true);
-
-  cursor_set_hidden(true);
-
-  lv_demo_benchmark();
-
-  while(1) {
-      /* Periodically call the lv_task handler.
-       * It could be done in a timer interrupt or an OS task too.*/
-      lv_timer_handler();
-      usleep(1 * 1000);
-  }
-
-  return 0;
 }
 
 /**********************
@@ -99,72 +98,69 @@ int main(int argc, char **argv)
 
 uint32_t custom_tick_get(void)
 {
-  struct timespec ts;
-
-  clock_gettime(CLOCK_MONOTONIC, &ts);
-
-  uint32_t tick = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
-
-  return tick;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    uint32_t tick = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+    return tick;
 }
 
 static void lv_port_disp_init(void)
 {
-  /*Linux frame buffer device init*/
-  fbdev_init();
+    /*Linux frame buffer device init*/
+    fbdev_init();
 
-  uint32_t width, height;
-  fbdev_get_sizes(&width, &height);
+    uint32_t width, height;
+    fbdev_get_sizes(&width, &height);
 
-  printf("fbdev: %" PRIu32 " x %" PRIu32 "\n", width, height);
+    printf("fbdev: %" PRIu32 " x %" PRIu32 "\n", width, height);
 
-  /*A small buffer for LittlevGL to draw the screen's content*/
-  uint32_t buf_size = width * height;
-  lv_color_t * buf = malloc(buf_size * sizeof(lv_color_t));
-  LV_ASSERT_MALLOC(buf);
+    /*A small buffer for LittlevGL to draw the screen's content*/
+    uint32_t buf_size = width * height;
+    lv_color_t* buf = malloc(buf_size * sizeof(lv_color_t));
+    LV_ASSERT_MALLOC(buf);
 
-  /*Initialize a descriptor for the buffer*/
-  static lv_disp_draw_buf_t disp_buf;
-  lv_disp_draw_buf_init(&disp_buf, buf, NULL, buf_size);
+    /*Initialize a descriptor for the buffer*/
+    static lv_disp_draw_buf_t disp_buf;
+    lv_disp_draw_buf_init(&disp_buf, buf, NULL, buf_size);
 
-  /*Initialize and register a display driver*/
-  static lv_disp_drv_t disp_drv;
-  lv_disp_drv_init(&disp_drv);
-  disp_drv.draw_buf   = &disp_buf;
-  disp_drv.flush_cb   = fbdev_flush;
-  disp_drv.hor_res    = width;
-  disp_drv.ver_res    = height;
-  lv_disp_drv_register(&disp_drv);
+    /*Initialize and register a display driver*/
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.draw_buf = &disp_buf;
+    disp_drv.flush_cb = fbdev_flush;
+    disp_drv.hor_res = width;
+    disp_drv.ver_res = height;
+    lv_disp_drv_register(&disp_drv);
 }
 
-static void lv_port_indev_init(const char * path, bool show_cursor)
+static void lv_port_indev_init(const char* path, bool show_cursor)
 {
-  evdev_init();
-  evdev_set_file(path);
+    evdev_init();
+    evdev_set_file(path);
 
-  static lv_indev_drv_t indev_drv;
+    static lv_indev_drv_t indev_drv;
 
-  /*Register a encoder input device*/
-  lv_indev_drv_init(&indev_drv);
-  indev_drv.type = LV_INDEV_TYPE_POINTER;
-  indev_drv.read_cb = evdev_read;
-  lv_indev_t * indev = lv_indev_drv_register(&indev_drv);
+    /*Register a encoder input device*/
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = evdev_read;
+    lv_indev_t* indev = lv_indev_drv_register(&indev_drv);
 
-  if(show_cursor) {
-    /*Set a cursor for the mouse*/
-    LV_IMG_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
-    lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
-    lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-    lv_indev_set_cursor(indev, cursor_obj);             /*Connect the image  object to the driver*/
-  }
+    if (show_cursor) {
+        /*Set a cursor for the mouse*/
+        LV_IMG_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
+        lv_obj_t* cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
+        lv_img_set_src(cursor_obj, &mouse_cursor_icon); /*Set the image source*/
+        lv_indev_set_cursor(indev, cursor_obj); /*Connect the image  object to the driver*/
+    }
 }
 
 static void cursor_set_hidden(bool en)
 {
-  // hide cursor echo -e "\033[?25l"
-  // show cursor echo -e "\033[?25h"
-  int ret = system(en ? "echo -e \"\033[?25l\"" : "echo -e \"\033[?25h\"");
-  printf(__func__);
-  printf(ret == 0 ? " OK" : " ERROR");
-  printf("\n");
+    // hide cursor echo -e "\033[?25l"
+    // show cursor echo -e "\033[?25h"
+    int ret = system(en ? "echo -e \"\033[?25l\"" : "echo -e \"\033[?25h\"");
+    printf(__func__);
+    printf(ret == 0 ? " OK" : " ERROR");
+    printf("\n");
 }
